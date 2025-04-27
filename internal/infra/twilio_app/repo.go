7ef2ed2585c6112151c25f3d7f50a4d5059ee14c
bhushan-aruto <nagabhushanbhandary524@gmail.com>
2025-04-27 23:59:20 +0530

    call feature updated

--- internal/infra/twilio_app/repo.go
+++ internal/infra/twilio_app/repo.go
@@ -1,25 +1,40 @@
 package twilio_app
 
-import api "github.com/twilio/twilio-go/rest/api/v2010"
+import (
+	"time"
+
+	api "github.com/twilio/twilio-go/rest/api/v2010"
+)
 
 type twilioRepo struct {
+	callStatus   bool
 	twilioClient *twilioClient
 }
 
 func NewTwilioRepo(twilioClient *twilioClient) *twilioRepo {
 	return &twilioRepo{
+		callStatus:   true,
 		twilioClient: twilioClient,
 	}
 }
 
 func (repo *twilioRepo) MakeAlertCall(callAnswerApiUrl, callFrom, callTo string) error {
-	params := new(api.CreateCallParams)
+	if repo.callStatus {
+		repo.callStatus = false
 
-	params.SetUrl(callAnswerApiUrl)
-	params.SetTo(callTo)
-	params.SetFrom(callFrom)
+		go func() {
+			time.Sleep(5 * time.Minute)
+			repo.callStatus = true
+		}()
 
-	_, err := repo.twilioClient.client.Api.CreateCall(params)
+		params := new(api.CreateCallParams)
+		params.SetUrl(callAnswerApiUrl)
+		params.SetTo(callTo)
+		params.SetFrom(callFrom)
+		_, err := repo.twilioClient.client.Api.CreateCall(params)
 
-	return err
+		return err
+	}
+
+	return nil
 }
